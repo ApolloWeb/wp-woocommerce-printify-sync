1a22e9eb940ca54b0275224cfb30e31d71294235
Rob Owen <rob.owen@apollo-web.co.uk>
2025-02-20 07:24:39 +0000

    Bug Fixes

--- wp-woocommerce-printify-sync.php
+++ wp-woocommerce-printify-sync.php
@@ -15,54 +15,53 @@ use ApolloWeb\WooCommercePrintifySync\Admin;
 use ApolloWeb\WooCommercePrintifySync\OrderSync;
 
 // Exit if accessed directly.
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 // Define plugin constants.
-define('WPS_PLUGIN_DIR', plugin_dir_path(__FILE__));
-define('WPS_PLUGIN_URL', plugin_dir_url(__FILE__));
+define( 'WPS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'WPS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-// Autoload classes.
-spl_autoload_register(function ($class) {
-    $prefix = __NAMESPACE__ . '\\';
-    $base_dir = __DIR__ . '/includes/';
-
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+// Autoload classes. This autoloader will look in both /includes and /admin directories.
+spl_autoload_register( function ( $class ) {
+	$prefix = __NAMESPACE__ . '\\';
+	$len    = strlen( $prefix );
+	if ( strncmp( $prefix, $class, $len ) !== 0 ) {
+		return;
+	}
+	$relative_class = substr( $class, $len );
+	$directories    = array(
+		__DIR__ . '/includes/',
+		__DIR__ . '/admin/',
+	);
+	foreach ( $directories as $base_dir ) {
+		$file = $base_dir . str_replace( '\\', '/', $relative_class ) . '.php';
+		if ( file_exists( $file ) ) {
+			require $file;
+			return;
+		}
+	}
+} );
 
 // Initialize the plugin.
-class Plugin
-{
-    public function __construct()
-    {
-        $this->initHooks();
-    }
+class Plugin {
+	public function __construct() {
+		$this->initHooks();
+	}
 
-    private function initHooks()
-    {
-        add_action('plugins_loaded', [$this, 'init']);
-    }
+	private function initHooks() {
+		add_action( 'plugins_loaded', [ $this, 'init' ] );
+	}
 
-    public function init()
-    {
-        if (is_admin()) {
-            new Admin();
-        }
-
-        new OrderSync();
-    }
+	public function init() {
+		if ( is_admin() ) {
+			new Admin();
+		}
+		new OrderSync();
+	}
 }
 
 // Initialize the plugin.
 new Plugin();
+?>
