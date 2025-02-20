8a4b276b1530010dc4d17b040daa9a8f903cf89d
Rob Owen <rob.owen@apollo-web.co.uk>
2025-02-20 05:46:23 +0000

    Cleared

--- assets/js/admin.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const shopSelect = document.querySelector('select[name="printify_selected_shop"]');
-
-    if (shopSelect) {
-        shopSelect.addEventListener('change', function () {
-            document.forms[0].submit();
-        });
-    }
-});
