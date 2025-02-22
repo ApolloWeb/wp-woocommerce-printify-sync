edb9150bc3cd4c5f89bb14178820a1f69ae28b9e
Rob Owen <rob.owen@apollo-web.co.uk>
2025-02-22 11:59:25 +0000

    Updated

---
+++ .github/workflows/.php-cs-fixer.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+ = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude('vendor')
+    ->notPath('wp-config.php');
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => ['default' => 'align_single_space'],
+        'blank_line_after_opening_tag' => true,
+        'braces' => true,
+        'concat_space' => ['spacing' => 'one'],
+    ])
+    ->setFinder();
---
+++ .github/workflows/eslint.config.js
@@ -0,0 +1,15 @@
+﻿import js from "@eslint/js";
+import recommended from "eslint-config-recommended";
+
+export default [
+  js.configs.recommended,
+  recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "indent": ["error", 2],
+      "quotes": ["error", "double"],
+    },
+  },
+];
