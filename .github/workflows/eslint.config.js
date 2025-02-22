2b892a937df9df7334d21b70534a2d767fc1591a
Rob Owen <rob.owen@apollo-web.co.uk>
2025-02-22 15:39:32 +0000

    Updated

--- .github/workflows/eslint.config.js
+++ .github/workflows/eslint.config.js
@@ -1,15 +1,22 @@
-﻿module.exports = {
-    env: {
-        node: true,
-        browser: true,
-        es2021: true,
+﻿// eslint.config.js
+export default [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                // If you need specific globals from `env`:
+                window: "readonly",
+                document: "readonly",
+                navigator: "readonly",
+                process: "readonly",
+            },
+        },
+        rules: {
+            "no-unused-vars": "error",
+            "eqeqeq": "error",
+        },
+        ignores: ["wp/", "vendor/", "node_modules/", ".github/"],
     },
-    extends: [
-        'eslint:recommended'
-    ],
-    rules: {
-        'no-unused-vars': 'error',
-        'eqeqeq': 'error'
-    },
-    ignorePatterns: ["wp/", "vendor/", "node_modules/", ".github/"],
-};
+];
