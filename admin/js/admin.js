46aa4095cd83c57c7b1fc49171001143299dbacd
Rob Owen <rob.owen@apollo-web.co.uk>
2025-03-02 02:43:10 +0000

    Automated commit: Changes made to files.

--- admin/js/admin.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-    "use strict";
-
-    $(document).ready(function() {
-        console.log("WP WooCommerce Printify Sync Admin JS loaded.");
-    });
-})(jQuery);
--- public/js/public.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-    "use strict";
-
-    $(document).ready(function() {
-        console.log("WP WooCommerce Printify Sync Public JS loaded.");
-    });
-})(jQuery);
