689ca27e6b5ef3fc4951d9b7fdba03ca1185fca8
Rob Owen <rob.owen@apollo-web.co.uk>
2025-03-06 05:13:25 +0000

    Initial Commit

--- assets/js/menu-icon.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    var menuIcon = $('#toplevel_page_wpwprintifysync .dashicons-store');
-    if (menuIcon.length) {
-        menuIcon.removeClass('dashicons dashicons-store');
-        menuIcon.addClass('fas fa-tshirt');
-    }
-});
