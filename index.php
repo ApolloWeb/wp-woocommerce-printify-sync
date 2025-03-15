5303e1a048a9a02582605e9e855835b295125578
Rob Owen <rob.owen@apollo-web.co.uk>
2025-03-15 03:37:41 +0000

    Updated

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-// Force HTTPS if not already using HTTPS
-    if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] !== 'https') {
-        $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        header("Location: " . $redirect_url, true, 301);
-        exit();
-}
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ */
 
-phpinfo();
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ */
+define('WP_USE_THEMES', true);
 
-?>
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp/wp-blog-header.php';
--- wp-config.php
+++ wp-config.php
@@ -1,21 +1,33 @@
 <?php
 
-require_once(dirname(__FILE__) . '/wp-load.php');
-require_once(dirname(__FILE__) . '/wp-includes/plugin.php');
-require_once(dirname(__FILE__) . '/wp-includes/pluggable.php');
+require_once __DIR__ . '/vendor/autoload.php';
 
-// Force HTTPS when using Ngrok
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
+
+require_once(dirname(__FILE__) . '/wp/wp-load.php');
+
+// Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    $_SERVER['HTTPS'] = 'on';
+    $_SERVER['HTTPS']  = 'on';
+}
+
+// Handle ngrok domain
+if (isset($_SERVER['HTTP_X_ORIGINAL_HOST'])) {
+    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_ORIGINAL_HOST'];
 }
 
 // Load environment variables
 
 /** MySQL settings */
-define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
-define('DB_USER', getenv('WORDPRESS_DB_USER'));
-define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
-define('DB_HOST', getenv('WORDPRESS_DB_HOST'));
+define('DB_NAME', getenv('WP_DB_NAME'));
+define('DB_USER', getenv('WP_DB_USER'));
+define('DB_PASSWORD', getenv('WP_DB_PASSWORD'));
+define('DB_HOST', getenv('WP_DB_HOST'));
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
 
 /** Authentication Unique Keys and Salts. */
 define('AUTH_KEY', getenv('AUTH_KEY'));
@@ -28,7 +40,7 @@ define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
 define('NONCE_SALT', getenv('NONCE_SALT'));
 
 /** WordPress Database Table prefix. */
-$table_prefix = 'wp_';
+$table_prefix = getenv('WP_TABLE_PREFIX');
 
 /** For developers: WordPress debugging mode. */
 define('WP_DEBUG', getenv('WP_DEBUG') === 'true');
@@ -52,7 +64,6 @@ define('WP_MAX_MEMORY_LIMIT', getenv('WP_MAX_MEMORY_LIMIT'));
 define('WORDPRESS_PLUGINS', getenv('WORDPRESS_PLUGINS'));
 define('WORDPRESS_SKIP_PLUGINS', getenv('WORDPRESS_SKIP_PLUGINS'));
 
-
 // SMTP Mailer Configuration (Outgoing Emails)
 define('SMTP_HOST', getenv('SMTP_HOST'));
 define('SMTP_PORT', getenv('SMTP_PORT'));
@@ -63,25 +74,29 @@ define('SMTP_AUTH', getenv('SMTP_AUTH') === 'true');
 define('SMTP_FROM', getenv('SMTP_FROM_EMAIL'));
 define('SMTP_NAME', getenv('SMTP_FROM_NAME'));
 
-// Force SMTP settings in WordPress
-add_action('phpmailer_init', function ($phpmailer) {
-    $phpmailer->isSMTP();
-    $phpmailer->Host = SMTP_HOST;
-    $phpmailer->Port = SMTP_PORT;
-    $phpmailer->SMTPAuth = SMTP_AUTH;
-    $phpmailer->Username = SMTP_USER;
-    $phpmailer->Password = SMTP_PASS;
-    $phpmailer->SMTPSecure = SMTP_SECURE;
-    $phpmailer->From = SMTP_FROM;
-    $phpmailer->FromName = SMTP_NAME;
-});
-
 // Postie (Incoming Mail via POP3)
 define('POSTIE_MAIL_SERVER', getenv('POP3_HOST'));
 define('POSTIE_MAIL_PORT', getenv('POP3_PORT'));
 define('POSTIE_MAIL_USER', getenv('POP3_USERNAME'));
 define('POSTIE_MAIL_PASSWORD', getenv('POP3_PASSWORD'));
 define('POSTIE_USE_SSL', getenv('POP3_SSL') === 'true');
+
+// Define WP_CONTENT directory to be at the root while core files remain in wp folder
+define('WP_CONTENT_DIR', dirname(__FILE__) . '/' . getenv('WP_CONTENT_DIR'));
+define('WP_CONTENT_URL', getenv('WP_CONTENT_URL'));
+
+// Redis configuration
+define('WP_REDIS_HOST', getenv('REDIS_HOST'));
+define('WP_REDIS_PORT', getenv('REDIS_PORT'));
+define('WP_REDIS_PASSWORD', getenv('REDIS_PASSWORD'));
+define('WP_REDIS_CLIENT', getenv('WP_REDIS_CLIENT'));
+define('WP_REDIS_DATABASE', getenv('WP_REDIS_DATABASE'));
+define('WP_REDIS_TIMEOUT', getenv('WP_REDIS_TIMEOUT'));
+define('WP_REDIS_READ_TIMEOUT', getenv('WP_REDIS_READ_TIMEOUT'));
+define('WP_REDIS_EXPIRE', getenv('WP_REDIS_EXPIRE'));
+define('WP_REDIS_MAXTTL', getenv('WP_REDIS_MAXTTL'));
+define('WP_CACHE', getenv('WP_CACHE'));
+define('WP_ENABLE_REDIS_CACHE', getenv('WP_ENABLE_REDIS_CACHE') === 'true');
 
 /** Absolute path to the WordPress directory. */
 if (!defined('ABSPATH')) {
