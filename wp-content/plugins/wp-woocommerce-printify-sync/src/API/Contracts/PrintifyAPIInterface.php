91a426913eb36578919aa24f96f7d378ae0e2f76
Rob Owen <rob.owen@apollo-web.co.uk>
2025-03-19 18:38:25 +0000

    Refactor WP WooCommerce Printify Sync plugin: remove obsolete files, update .gitignore, and clean up code structure

--- wp-content/plugins/wp-woocommerce-printify-sync/src/API/Contracts/PrintifyAPIInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ApolloWeb\WPWooCommercePrintifySync\API\Contracts;
-
-interface PrintifyAPIInterface {
-    public function getShops(): array;
-    public function getProducts(string $shop_id): array;
-    public function createProduct(string $shop_id, array $product_data): array;
-}
--- wp-content/plugins/wp-woocommerce-printify-sync/src/Http/Contracts/HttpClientInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ApolloWeb\WPWooCommercePrintifySync\Http\Contracts;
-
-interface HttpClientInterface {
-    public function request(string $url, string $method = 'GET', array $options = []): array;
-}
