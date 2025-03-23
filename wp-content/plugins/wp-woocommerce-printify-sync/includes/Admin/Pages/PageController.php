962e35c83d016e98170952826e9413353769d24e
Rob Owen <rob.owen@apollo-web.co.uk>
2025-03-23 19:29:48 +0000

    Remove deprecated templates and files from the WooCommerce Printify Sync plugin
    - Deleted AI content suggestions template: `product-ai-meta-box.php`
    - Removed product management template: `products.php`
    - Eliminated email signature settings template: `settings-signature.php`
    - Removed main settings template: `settings.php`
    - Deleted shipping settings template: `shipping-settings.php`
    - Removed sync settings template: `sync-settings.php`
    - Deleted default email template: `default.php`
    - Removed PHPUnit bootstrap file: `bootstrap.php`
    - Added new environment configuration file: `wp-env.json`

--- wp-content/plugins/wp-woocommerce-printify-sync/includes/Admin/Pages/PageController.php
+++
@@ -1,9 +0,0 @@
-// ...existing code...
-protected function getTemplate(): string {
-    return str_replace(
-        '/home/apolloweb/projects/wp-woocommerce-printify-sync/',
-        '/home/apolloweb/projects/wp-woocommerce-printify-sync/wp-content/plugins/wp-woocommerce-printify-sync/',
-        $this->template
-    );
-}
-// ...existing code...
