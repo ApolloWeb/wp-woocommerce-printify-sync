959b3d9ce1167e0f6ae5d5d68287e4ea821380bb
Rob Owen <rob.owen@apollo-web.co.uk>
2025-03-24 14:49:48 +0000

    Remove deprecated classes and templates related to encryption and rendering. This includes the EncryptionService, Template Engine, TemplateDebugger, and associated settings and dashboard templates. Transition to a more streamlined approach for handling settings and dashboard rendering without the use of the previous template engine.

--- wp-content/plugins/wp-woocommerce-printify-sync/src/Admin/Interfaces/SettingsInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ApolloWeb\WPWooCommercePrintifySync\Admin\Interfaces;
-
-interface SettingsInterface {
-    public function init(): void;
-    public function renderDashboard(): void;
-    public function renderSettings(): void;
-}
