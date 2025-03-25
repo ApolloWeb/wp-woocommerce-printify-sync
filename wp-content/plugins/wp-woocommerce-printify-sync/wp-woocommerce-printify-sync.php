43e3c08ffc3cbe3ab1d9f0120a71e3b2f81e6ff6
Rob Owen <rob.owen@apollo-web.co.uk>
2025-03-25 21:56:52 +0000

    Updated

---
+++ wp-content/plugins/wp-woocommerce-printify-sync/wp-woocommerce-printify-sync.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WP WooCommerce Printify Sync
+ * Description: Sync products from Printify to WooCommerce
+ * Plugin URI: https://github.com/ApolloWeb/wp-woocommerce-printify-sync
+ * Version: 1.0.0
+ * Author: ApolloWeb
+ * Author URI: https://github.com/ApolloWeb
+ * Text Domain: wp-woocommerce-printify-sync
+ * Domain Path: /languages
+ * Requires at least: 5.6
+ * Requires PHP: 7.3
+ * License: MIT
+ *
+ * @package ApolloWeb\WPWooCommercePrintifySync
+ */
